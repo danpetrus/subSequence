c40d53ff9827aaa2cfbde39e0d3398ea5d56a513
Dan-Mihai Petrus <34897927+danpetrus@users.noreply.github.com>
2021-02-17 11:24:24 +0200

    Add files via upload

---
+++ SubSequenceArr/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SubSequenceArr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 5, 1, 22, 25, 6, -1, 8, 10 };
+            int[] B = { 1, 6, -1, 10 };
+            int x = 0;
+            bool match = false;
+
+            foreach (int i in B)
+            {
+                match = false;
+
+                while (x < A.Length)
+                {
+                    Console.WriteLine("Comparing " + i + " with " + A[x] + " | ");
+                    if (i == A[x])
+                    {
+                        x++;
+                        match = true;
+                        break;
+                    }
+                    x++;
+                }
+            }
+
+            if (match == false)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("B IS NOT Subsequence of A");
+            }
+            else 
+            {
+                Console.WriteLine("");
+                Console.WriteLine("B IS Subsequence of A");
+            }
+        }
+    }
+}
